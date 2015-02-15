fe010b52f081e70b7711d8cf20c052a9a547f2d1
Osmar Valero <osmarvalero100@gmail.com>
2015-02-15 13:44:41 -0500

    Agregando registros a la base de datos

--- facturas.sql
+++ facturas.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-02-2015 a las 17:58:38
+-- Tiempo de generación: 15-02-2015 a las 19:42:17
 -- Versión del servidor: 5.6.21
 -- Versión de PHP: 5.6.3
 
@@ -33,6 +33,13 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `nit` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`codcliente`, `nombre`, `direccion`, `nit`) VALUES
+('001', 'Lucas Shoes LTDA', 'Calle 59 # 20 -20', '5635656');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +51,13 @@ CREATE TABLE IF NOT EXISTS `detalle` (
   `idproducto` varchar(5) DEFAULT NULL,
   `cantidad` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `detalle`
+--
+
+INSERT INTO `detalle` (`codFactura`, `idproducto`, `cantidad`) VALUES
+('c001', 'p001', 5);
 
 -- --------------------------------------------------------
 
@@ -58,6 +72,33 @@ CREATE TABLE IF NOT EXISTS `factura` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Volcado de datos para la tabla `factura`
+--
+
+INSERT INTO `factura` (`idfactura`, `idcliente`, `fecha`) VALUES
+('C001', '001', '2015-01-13');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE IF NOT EXISTS `producto` (
+  `codproducto` varchar(5) NOT NULL,
+  `idnombre` varchar(15) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codproducto`, `idnombre`, `precio`) VALUES
+('p001', 'TABLET ASSUS 20', 299900),
+('p002', 'SPEAKERS B-79', 25000);
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -68,10 +109,39 @@ ALTER TABLE `cliente`
  ADD PRIMARY KEY (`codcliente`);
 
 --
+-- Indices de la tabla `detalle`
+--
+ALTER TABLE `detalle`
+ ADD KEY `detalle_producto_FK1` (`idproducto`), ADD KEY `detalle_factura_FK1` (`codFactura`);
+
+--
 -- Indices de la tabla `factura`
 --
 ALTER TABLE `factura`
- ADD PRIMARY KEY (`idfactura`);
+ ADD PRIMARY KEY (`idfactura`), ADD KEY `factura_FK1` (`idcliente`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+ ADD PRIMARY KEY (`codproducto`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle`
+--
+ALTER TABLE `detalle`
+ADD CONSTRAINT `detalle_factura_FK1` FOREIGN KEY (`codFactura`) REFERENCES `factura` (`idfactura`),
+ADD CONSTRAINT `detalle_producto_FK1` FOREIGN KEY (`idproducto`) REFERENCES `producto` (`codproducto`);
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+ADD CONSTRAINT `factura_FK1` FOREIGN KEY (`idcliente`) REFERENCES `cliente` (`codcliente`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
